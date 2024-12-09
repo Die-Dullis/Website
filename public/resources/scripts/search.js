6414a3ed1c27b3953fa0e1e134afca127d12aec9
Simon Luginbuehl <simon.luginbuehl@ict-campus.net>
2024-12-09 11:14:24 +0100

    search.js

--- public/resources/scripts/search.js
+++ public/resources/scripts/search.js
@@ -12,3 +12,20 @@ function searchText() {
         }
     });
 }
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('searchInput').addEventListener('input', function () {
+        const searchValue = this.value.trim().toLowerCase();
+        const cards = document.querySelectorAll('.Blog .post');
+
+        cards.forEach(card => {
+            const projectName = card.querySelector('#title').textContent.trim().toLowerCase();
+            if (projectName.includes(searchValue)) {
+                card.style.display = 'flex';
+            } else {
+                card.style.display = 'none';
+            }
+        });
+    });
+});
