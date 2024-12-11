3b807237ed9b78e57f4da12f5f5978826c7b2f22
Simon Luginbuehl <simon.luginbuehl@ict-campus.net>
2024-12-11 11:18:04 +0100

    Fixed Immage slider
    Signed-off-by: Simon Luginbuehl <simon.luginbuehl@ict-campus.net>

--- public/resources/scripts/diashow.js
+++ public/resources/scripts/diashow.js
@@ -1,43 +1,53 @@
-let slideIndex = 1;
+let list = document.querySelector('.slider .list');
+let items = document.querySelectorAll('.slider .list .item');
+let dots = document.querySelectorAll('.slider .dots li');
+let prev = document.getElementById('prev');
+let next = document.getElementById('next');
+
+let active = 0;
+let lengthitems = items.length - 1;
 let alredyclicked = false;
-showSlides();
 
-
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-    alredyclicked = true;
-}
-  
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-    alredyclicked = true;
+next.onclick = function() {
+    if (active + 1 > lengthitems) {
+        active = 0;
+    } else {
+        active = active + 1;
+    }
+    alredyclicked = true
+    reloadslider();
 }
 
-function timer(){
-    setTimeout(1000); // Change image every 4 seconds
-    if (alredyclicked = flase){
-        showSlides();
+prev.onclick = function() {
+    if (active - 1 < 0) {
+        active = lengthitems;
+    } else {
+        active = active - 1;
+    }
+    alredyclicked = true
+    reloadslider();
+}
+let autoslide = setInterval(() => { 
+    if (alredyclicked === false){
+        next.click();
     }
     else{
-        alredyclicked = false
-    }
+        alredyclicked = false 
+    }}, 3000);
 
-}
-function showSlides(n) {
-    let i;
-    let slides = document.getElementsByClassName("mySlides");
-    let dots = document.getElementsByClassName("dot");
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += " active";
-    
-    timer() 
+function reloadslider() {
+    let checkleft = items[active].offsetLeft;
+    list.style.left = -checkleft + 'px';
+
+    let lastactiveDot = document.querySelector('.slider .dots li.active');
+    if (lastactiveDot) lastactiveDot.classList.remove('active');
+    dots[active].classList.add('active');
 }
 
+dots.forEach((li, key) => {
+    li.addEventListener('click', function() {
+        active = key;
+        alredyclicked = true
+        reloadslider();
+    })
+})
